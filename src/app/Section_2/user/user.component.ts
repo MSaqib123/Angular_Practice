57c2ef4a1c702a1ea31170557fcfac92ca59cd5b
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-11 07:42:56 +0500

    practice practice just practice

--- src/app/Section_2/user/user.component.ts
+++ src/app/Section_2/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, output, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { User } from '../User.model';
 
 @Component({
   selector: 'app-user',
@@ -9,49 +8,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
-  // // selectUser = DUMMY_USERS[0]
-  // selectUser = DUMMY_USERS[randomIndex]
+export class UserComponent {  
+  // @Input({required:true}) user!:User;
+  user = input<User>();
+  @Input({required:true})  selectedUser!: boolean;
+  select = output<string>();
 
-  // //==== property getter =====
-  // get userImg(){
-  //   return 'assets/users/'+this.selectUser.avatar
-  // }
+  userImg = computed(() => 'assets/users/'+this.user()!.avatar);
 
-  // changeUser(){
-  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-  //   this.selectUser = DUMMY_USERS[randomIndex];
-  // }
-
-  
-
-
-
-  // selectUser = DUMMY_USERS[0]
-  selectUser =signal(DUMMY_USERS[randomIndex] )
-
-  //==== property getter =====\
-  userImg = computed(()=> 'assets/users/' + this.selectUser().avatar)
-  // get userImg(){
-  //   return 'assets/users/'+this.selectUser.avatar
-  // }
-
-  changeUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectUser.set(DUMMY_USERS[randomIndex])
-  }
-
-  
-
-
-  // Note  benifit of signla 
-  // signal jis component ya jis  jagaa   change ataa ha use ke update  angular ko data
-  // langke 
-
-  // Zone.js  ya  1 jaga chage att tha to sb me changing ke update angular ko data thaha
-  // signals
-
-
-
-  
+  onSelectUser(){
+    this.select.emit(this.user()!.id);
+  }  
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Section_2/header/header.component';
 import { UserComponent } from './Section_2/user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,13 @@ import { UserComponent } from './Section_2/user/user.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+
+  users = DUMMY_USERS;
+  selectedUserId = "u1";
+  onSelectUser(id:string){
+    this.selectedUserId = id;
+    
+  }
+
+
 }
