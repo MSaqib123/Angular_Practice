2dcd79465d30f048232f712092441cb395ab9a58
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-11 08:04:49 +0500

    practice make the person perfect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Section_2/header/header.component';
 import { UserComponent } from './Section_2/user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./Section_2/tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   users = DUMMY_USERS;
-  selectedUserId = "u1";
+  selectedUserId = "";
   onSelectUser(id:string){
     this.selectedUserId = id;
     
   }
-
+  get onSelectedUser(){
+    return this.users.find(x=>x.id == this.selectedUserId)
+  }
 
 }
